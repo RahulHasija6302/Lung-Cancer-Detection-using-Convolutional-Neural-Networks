3a3fef6ade92e3d9c94d2cf0c45e87a42bea4b78
Rahul Hasija <106419873+RahulHasija6302@users.noreply.github.com>
2023-10-21 11:16:32 +0530

    Create results.py

---
+++ results.py
@@ -0,0 +1,14 @@
+
+
+
+//Results and Discussions
+
+
+
+
+# Discuss the results obtained from the trained model and potential areas for improvement
+print("Accuracy: {:.4f}".format(history.history["accuracy"][-1]))
+print("Validation Accuracy: {:.4f}".format(history.history["val_accuracy"][-1]))
+
+# Analyze any challenges faced during the project and potential reasons behind them
+# Suggest areas for improvement or future research directions
